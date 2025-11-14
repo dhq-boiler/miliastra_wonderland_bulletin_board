f99cb02f735a6d5e083cd1e0db367a08adf932f0
dhq_boiler <dhq_boiler@live.jp>
2025-11-15 02:19:03 +0900

    Update updated_at when tag update

--- app/models/stage_tag.rb
+++ app/models/stage_tag.rb
@@ -1,7 +1,7 @@
 class StageTag < ApplicationRecord
   include SoftDeletable
 
-  belongs_to :stage
+  belongs_to :stage, touch: true
   belongs_to :tag
 
   validates :stage_id, uniqueness: { scope: :tag_id }
